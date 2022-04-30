228907cebb5270f4c7872070e0b4c80fc89a16ca
pandasrcute <tzhaobhs@gmail.com>
2022-04-30 03:53:07 -0700

    set up JUnit

--- Age.java
+++ Age.java
@@ -1,3 +1,12 @@
 public class Age {
 
+   // Calculates and returns age
+   // If currentYear is less than birthYear, it will return 0
+   public static int ageCalculator(int currentYear, int birthYear) {
+
+      int age = currentYear - birthYear;
+
+      return age;
+   } 
+
 }
---
+++ AgeTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class AgeTest {
+   @Test
+   public void testAgeCalculator() {
+   assertEquals(0, Age.ageCalculator(2003,2022)); //should fail
+   }
+}
